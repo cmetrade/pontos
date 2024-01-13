0bb1a5fb2e461d2eb87dc047dc403831d11b5d23
cmetrade <cmetrade.6l@gmail.com>
2024-01-13 20:37:34 -0300

    update DX

--- cypress/e2e/dx.cy.js
+++ cypress/e2e/dx.cy.js
@@ -9,18 +9,13 @@ describe('Login to investing and get DX', function (){
         
             cy.get('.pid-8827-low').then(function(text1){
                 cy.log(text1.text())
-                cy.writeFile('dx-min.txt', text1.text())
+                cy.writeFile('arquivos/dx-min.txt', text1.text())
            })
 
            cy.get('.pid-8827-high').then(function(text2){
             cy.log(text2.text())
-            cy.writeFile('dx-max.txt', text2.text())
+            cy.writeFile('arquivos/dx-max.txt', text2.text())
            })
-           /*
-           cy.once("fail", (err) =>
-           {
-               return false;
-           });
-           */
+
     })
 })
