4992e04f8acc54df6a954602cb71f655cd2d209c
cmetrade <cmetrade.6l@gmail.com>
2024-01-13 21:31:06 -0300

    update

--- cypress/e2e/xau.cy.js
+++ cypress/e2e/xau.cy.js
@@ -1,15 +1,15 @@
 /// <reference types="cypress" />
-describe('Login to investing and get XAU ', function (){
+describe('XAU ', function (){
 
     it('capturar dados xau', function(){
        cy.visit('https://br.investing.com/currencies/xau-usd-historical-data')
             cy.get('tbody > :nth-child(1) > :nth-child(5)').then(function(text2){
                 cy.log(text2.text())
-                cy.writeFile('xau-min.txt', text2.text(), {flag: "a+"})
+                cy.writeFile('arquivos/xau-min.txt', text2.text(), {flag: "a+"})
            })
             cy.get('.p-0 > .w-full > tbody > :nth-child(1) > :nth-child(4)').then(function(text3){
                 cy.log(text3.text())
-                cy.writeFile('xau-max.txt', text3.text(), {flag: "a+"})
+                cy.writeFile('arquivos/xau-max.txt', text3.text(), {flag: "a+"})
             })
     })
 })
