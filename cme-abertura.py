e9843e5ce5afc5c0144b64266cf2a0e29d087657
cmetrade <cmetrade.6l@gmail.com>
2024-01-14 11:12:23 -0300

    busca

--- cme-abertura.py
+++ cme-abertura.py
@@ -2,14 +2,16 @@ import pandas as pd
 import time
 import os
 
-os.system("num=`awk {'print $1'} arquivos/cme-open.txt`")
-os.system("echo scale=3; 1/$num|bc > arquivos/cme-open-convert.txt")
-with open("arquivos/cme-open-convert.txt", "r") as arquivo:
-        result = arquivo.read()
-print(result)
+with open("arquivos/cme-open.txt", "r") as arquivo:
+        x = arquivo.read()
+
+var1 = float(x)
+number = 1 / var1
+result = round(number, 3) 
+
 import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
-message = "USD-BRL -> "
+message = "CME ABERTURA - "
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
 print(requests.get(url).json()) # this sends the message
