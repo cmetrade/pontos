e8f59748eea6ab6f64f1418afd2f4d5f7d0053dc
cmetrade <cmetrade.6l@gmail.com>
2024-01-14 18:29:01 -0300

    busca

--- cypress/e2e/dx.cy.js
+++ cypress/e2e/dx.cy.js
@@ -2,7 +2,7 @@
 
 
 describe('DX', function (){
-    Cypress.config('defaultCommandTimeout', 10000);
+    Cypress.config('defaultCommandTimeout', 20000);
     it('DX', function(){
         cy.visit('https://br.investing.com/currencies/us-dollar-index-historical-data')
         
@@ -15,6 +15,11 @@ describe('DX', function (){
            cy.get('.pid-8827-high').then(function(text2){
             cy.log(text2.text())
             cy.writeFile('arquivos/dx-max.txt', text2.text())
+           })
+
+           cy.get('.bold > :nth-child(1) > [dir="ltr"]').then(function(text3){
+            cy.log(text3.text())
+            cy.writeFile('arquivos/dx-fechamento.txt', text3.text())
            })
 
     })
