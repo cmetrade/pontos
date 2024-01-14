c509485ce4e182f03569c5609938c548e0bc8871
cmetrade <cmetrade.6l@gmail.com>
2024-01-13 21:30:17 -0300

    update

--- cypress/e2e/usd-brl.cy.js
+++ cypress/e2e/usd-brl.cy.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
-describe('Login to investing', function (){
+describe('USD-BRL', function (){
 
-    it('capturar dados usd-brl', function(){
+    it('USD-BRL', function(){
        cy.visit('https://br.investing.com/currencies/usd-brl-historical-data')
 
             //abertura 
             cy.get('.p-0 > .w-full > tbody > :nth-child(1) > :nth-child(3)').then(function(text1){
             cy.log(text1.text())
-            cy.writeFile('usd-brl-abertura.txt', text1.text(), {flag: "a+"})
+            cy.writeFile('arquivos/usd-brl-abertura.txt', text1.text(), {flag: "a+"})
             })
 
             //maxima 
             cy.get('.p-0 > .w-full > tbody > :nth-child(1) > :nth-child(4)').then(function(text2){
             cy.log(text2.text())
-            cy.writeFile('usd-brl-max.txt', text2.text(), {flag: "a+"})
+            cy.writeFile('arquivos/usd-brl-max.txt', text2.text(), {flag: "a+"})
             })
 
             //minima 
             cy.get('.w-full > tbody > :nth-child(1) > :nth-child(5)').then(function(text3){
             cy.log(text3.text())
-            cy.writeFile('usd-brl-min.txt', text3.text(), {flag: "a+"})
+            cy.writeFile('arquivos/usd-brl-min.txt', text3.text(), {flag: "a+"})
             })
 
     })
