b05bb05cb281a06d831f2c5247b72aae6fbd206a
cmetrade <cmetrade.6l@gmail.com>
2024-01-13 21:30:53 -0300

    update

--- cypress/e2e/wdoG.cy.js
+++ cypress/e2e/wdoG.cy.js
@@ -1,14 +1,14 @@
 /// <reference types="cypress" />
 
 
-describe('get WDOG24', function (){
+describe('WDOG24', function (){
 
     it('capturar dados wdo', function(){
         cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOG24/cotacao')
         
             cy.get('#quoteElementPiece13').then(function(text2){
                cy.log(text2.text())
-               cy.writeFile('fechamentoWDO.txt', text2.text())
+               cy.writeFile('arquivos/fechamentoWDO.txt', text2.text())
            })
            
     })
