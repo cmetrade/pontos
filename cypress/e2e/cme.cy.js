d6a47335f680885e55d4deb8aa8da2c54c37fdd4
cmetrade <cmetrade.6l@gmail.com>
2024-01-13 21:29:22 -0300

    update CME

--- cypress/e2e/cme.cy.js
+++ cypress/e2e/cme.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-describe('Capture CME Yahoo finance', function (){
+describe('CME', function (){
 
     it('capturar dados cme', function(){
 
@@ -8,19 +8,19 @@ describe('Capture CME Yahoo finance', function (){
             // open
             cy.get('tbody > :nth-child(1) > :nth-child(2) > span').then(function(text1){
             cy.log(text1.text())
-            cy.writeFile('cme-open.txt', text1.text())
+            cy.writeFile('arquivos/cme-open.txt', text1.text())
             })
             
             //high
             cy.get('tbody > :nth-child(1) > :nth-child(3) > span').then(function(text2){
             cy.log(text2.text())
-            cy.writeFile('cme-high.txt', text2.text())
+            cy.writeFile('arquivos/cme-high.txt', text2.text())
             })
 
             //low
             cy.get('tbody > :nth-child(1) > :nth-child(4) > span').then(function(text3){
             cy.log(text3.text())
-            cy.writeFile('cme-low.txt', text3.text())
+            cy.writeFile('arquivos/cme-low.txt', text3.text())
             })
 
     })
