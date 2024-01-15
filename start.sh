bb85fa234590f959cca70e4ece1363ef729b13aa
cmetrade <cmetrade.6l@gmail.com>
2024-01-14 21:47:53 -0300

    xau dx

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./busca.sh
+python cme-abertura.py
+python cme-high.py
+python cme-low.py
+python cupom.py
+python usd-brl.py
+python dx-alta.py
+python dx-baixa.py
+python xau-alta.py
+python xau-baixa.py
+python maisum.py
+python menosum.py
