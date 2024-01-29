d48ba95020f79418ef060d8197fe47afb3a0f794
cmetrade <cmetrade.6l@gmail.com>
2024-01-29 14:22:30 -0300

    modified:   shs/busca.sh

--- shs/busca.sh
+++ shs/busca.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb bc
+#sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb bc
 cd ~/pontos
 rm arquivos/*.txt
 mv cypress/*.js cypress/e2e/
