68c42f6b096f8742507e20800d18a7e01a4c0cb0
cmetrade <cmetrade.6l@gmail.com>
2024-01-30 19:21:49 -0300

    abertura

--- shs/menosemais.sh
+++ shs/menosemais.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd ~/pontos
-i=1
 arquivo="arquivos/abrtura_d-1_WDO.txt"
 
-while [[ $i -lt 11 ]] 
+i=1
+
+while [[ $i -lt 60 ]] 
 do
-        if [ -e "$arquivo" ]
-           then
+        if [ -e $arquivo ]; then
            python3 pys/maisum.py
            python3 pys/menosum.py
            break
         fi
 echo "$i tentativa(s)"
-echo "$i tentativa(s)" > logs/menosemais.log
+echo "$i tentativa(s)" > logs/dx.log
 ((i++))
 done
