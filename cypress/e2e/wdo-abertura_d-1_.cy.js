526bb3921c60396af292b914bc59eb4be987c6df
cmetrade <cmetrade.6l@gmail.com>
2024-01-30 15:25:38 -0300

    abertura

--- cypress/e2e/wdo-abertura_d-1_.cy.js
+++ cypress/e2e/wdo-abertura_d-1_.cy.js
@@ -4,7 +4,7 @@ describe('WDOG - Abertura D -1', function (){
     it('capturar dados de abertura wdo', function(){
         cy.visit('https://br.investing.com/currencies/usd-brl-bmf-futures')
         
-        cy.get(':nth-child(4) > .float_lang_base_2').then(function(text2){
+        cy.get('.bold > :nth-child(2) > [dir="ltr"]').then(function(text2){
                cy.log(text2.text())
                cy.writeFile('arquivos/abertura_D-1_WDO.txt', text2.text())
            })
