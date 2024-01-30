a02280c3d8c23349ac6ead12fb087d3f1475ba8a
cmetrade <cmetrade.6l@gmail.com>
2024-01-30 20:32:10 -0300

    abertura

--- pys/maisum.py
+++ pys/maisum.py
@@ -3,7 +3,7 @@ import time
 import os
 
 
-with open("arquivos/abertura_D-1_WDO.txt", "r") as arquivo:
+with open("arquivos/fechamentoWDO.txt", "r") as arquivo:
         x = arquivo.read()
 
 wdo = float(x.replace('.','').replace(',','.'))
