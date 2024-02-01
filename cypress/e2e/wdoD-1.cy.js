abf7c9c53cd89a52c5d18a2240ead84993dfd130
cmetrade <cmetrade.6l@gmail.com>
2024-01-31 22:40:46 -0300

    1

--- cypress/e2e/wdoD-1.cy.js
+++ cypress/e2e/wdoD-1.cy.js
@@ -2,11 +2,11 @@
 
 describe('WDOG - Abertura D -1', function (){
     it('capturar dados de abertura wdo', function(){
-        cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOG24/cotacao')
-        
-        cy.get('#quoteElementPiece10').then(function(text2){
+        cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOH24/historico')
+       
+        cy.get(':nth-child(8) > table > tbody > :nth-child(3) > :nth-child(2)').then(function(text2){
               cy.log(text2.text())
-              cy.writeFile('arquivos/abertura_D-1_WDO.txt', text2.text())
+              cy.writeFile('arquivos/wdoD-1.txt', text2.text())
         })
           
     })
