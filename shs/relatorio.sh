f1e71db63786a8c20bb41ba75cc54fae445943c4
cmetrade <cmetrade.6l@gmail.com>
2024-02-02 20:32:01 -0300

    q

---
+++ shs/relatorio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd ~/pontos/logs
+cme=`cat cme.log`
+cupom=`cat cupom.log`
+dx=`cat dx.log`
+menosemais=`cat menosemais.log`
+usdbrl=`cat usd-brl.log`
+wdofecha=`cat wdo-fecha.log`
+wdoD1=`cat wdoD-1.log`
+xau=`cat xau.log`
+
+echo " " > relatorio.log
+echo " " >> relatorio.log
+echo "CME = $cme" >> relatorio.log
+echo "CUPOM = $cupom" >> relatorio.log
+echo "MENOS E MAIS = $menosemais" >> relatorio.log
+echo "USD/BRL = $usdbrl" >> relatorio.log
+echo "FECHAMENTO WDO = $wdofecha" >> relatorio.log
+echo "WDO D -1 = $wdoD1" >> relatorio.log
+echo "XAU = $xau" >> relatorio.log
+echo "DX = $dx" >> relatorio.log
+echo " " >> relatorio.log
+echo " " >> relatorio.log
