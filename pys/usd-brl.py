162fd76f9d81f9afedf49cf9aa13069be51903ed
cmetrade <cmetrade.6l@gmail.com>
2024-02-02 11:24:13 -0300

    q

--- pys/usd-brl.py
+++ pys/usd-brl.py
@@ -11,4 +11,4 @@ TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
 message = "USD-BRL -> "
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
-print(requests.get(url).json()) # this sends the message
+print(requests.get(url).json()) # this sends the message1
