2d90b32ce56578e5fe59248dd846c6d18d3e08e4
cmetrade <cmetrade.6l@gmail.com>
2024-02-03 12:45:53 -0300

    q

--- pys/dx-alta.py
+++ pys/dx-alta.py
@@ -29,6 +29,6 @@ result = rounded_number
 import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
-message = "DX ALTA = "
+message = "DX MÁXIMA = "
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
 print(requests.get(url).json()) # this sends the message
