1c5bb9eafa6c0d1389299afb9a2eafee13a93d14
cmetrade <cmetrade.6l@gmail.com>
2024-02-03 13:19:06 -0300

    q

--- pys/cupom.py
+++ pys/cupom.py
@@ -2,12 +2,12 @@ import pandas as pd
 import time
 import os
 
-with open("arquivos/cupom.txt", "r") as arquivo:
+with open("arquivos/cupom2.txt", "r") as arquivo:
         result = arquivo.read()
 print(result)
 import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
-message = "DOL Cupom -> "
+message = "DOL CUPOM = "
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
 print(requests.get(url).json()) # this sends the message
