998b18a926884a1a2d89627e2711dd7e4a0153c7
cmetrade <cmetrade.6l@gmail.com>
2024-02-04 13:47:03 -0300

    q

--- pys/ptax.py
+++ pys/ptax.py
@@ -8,6 +8,6 @@ print(result)
 import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
-message = "DOL CUPOM = "
+message = "PTAX = "
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
 print(requests.get(url).json()) # this sends the message
