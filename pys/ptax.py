c448cde9b29aa770d53f5c3db27f573d486776c2
cmetrade <cmetrade.6l@gmail.com>
2024-02-04 13:32:20 -0300

    q

--- pys/ptax.py
+++ pys/ptax.py
@@ -1,16 +1,13 @@
 import pandas as pd 
 import time
 import os
+
 with open("arquivos/ptax.txt", "r") as arquivo:
-        x = arquivo.read()
-
-var1 = float(x)
-number = 1 / var1
-result = round(number, 3) 
-
+        result = arquivo.read()
+print(result)
 import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
-message = "PTAX = "
+message = "DOL CUPOM = "
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
 print(requests.get(url).json()) # this sends the message
