188c30c730dcbd07cfd6965019e0a4ea5f43503c
cmetrade <cmetrade.6l@gmail.com>
2024-02-05 23:17:23 -0300

    q

--- shs/dx.sh
+++ shs/dx.sh
@@ -11,9 +11,13 @@ do
         npm start
         if [ "$?" == '0' ]; then
            if [ -s "arquivos/dx-fechamento.txt" ]; then
-              python3 pys/dx-alta.py
-              python3 pys/dx-baixa.py 
-           break
+              dxmax=`cat arquivos/dx-max.txt`
+              dxmin=`cat arquivos/dx-min.txt`
+                  if [ $dxmax != $dxmin ]; then
+                     python3 pys/dx-alta.py
+                     python3 pys/dx-baixa.py 
+                     break
+                  fi
            fi
         fi
 echo "$i tentativa(s)"
