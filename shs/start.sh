e2025cf2cc514cdf4aaf445e11ce142fa7ffaa2f
cmetrade <cmetrade.6l@gmail.com>
2024-02-05 23:17:06 -0300

    q

--- shs/start.sh
+++ shs/start.sh
@@ -4,14 +4,14 @@
 cd ~/pontos/shs
 rm ~/pontos/arquivos/*.txt
 rm ~/pontos/logs/*.log
+./wdo-fecha.sh
+./wdoD-1.sh
+./menosemais.sh
 ./ptax.sh
 ./cupom.sh
 ./usd-brl.sh
 ./cme.sh
-./wdo-fecha.sh
-./wdoD-1.sh
 ./xau.sh
 ./dx.sh
-./menosemais.sh
 ./relatorio-concat.sh
 ./relatorio.sh
