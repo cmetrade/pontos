94fbfed05ddb4fd5e2b1fc06e23ec51bb18cff40
cmetrade <cmetrade.6l@gmail.com>
2024-02-13 19:43:19 -0300

    q

--- shs/usd-brl.sh
+++ shs/usd-brl.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 cd ~/pontos
 mv cypress/e2e/* cypress/
+dia_semana="Tuesday"
 dia=`date +"%A"`
 
 
-if [[ $dia == "Monday" ]] ; then
+if [[ $dia == "$dia_semana" ]] ; then
    mv cypress/usd-brl-segunda.cy.js cypress/e2e/
    npm install
    i=1
         while [[ $i -lt 11 ]] 
         do
         npm start
-                if [ "$?" == '0' ]
-                then
-                        if [ -s "arquivos/usd-brl-abertura.txt" ];then
-                        python3 pys/usd-brl-segunda.py
-                        break
-                        fi
+                if [ "$?" == '0' ] then
+                python3 pys/usd-brl-segunda.py
+                break
                 fi
         echo "$i tentativa(s)"
         ((i++))
@@ -31,12 +29,9 @@ i=1
         while [[ $i -lt 11 ]] 
         do
         npm start
-        if [ "$?" == '0' ]
-           then
-                if [ -s "arquivos/usd-brl-abertura.txt" ];then
+        if [ "$?" == '0' ] ; then
                 python3 pys/usd-brl.py
                 break
-                fi
         fi
         echo "$i tentativa(s)"
         ((i++))
