d01925a82244b7e803876a6999b6e32f4ee8ae71
cmetrade <cmetrade.6l@gmail.com>
2024-02-13 13:14:55 -0300

    q

--- cypress/e2e/usd-brl-segunda.cy.js
+++ cypress/e2e/usd-brl-segunda.cy.js
@@ -7,19 +7,19 @@ describe('USD-BRL', function (){
             //abertura 
             cy.get('.p-0 > .w-full > tbody > :nth-child(2) > :nth-child(3)').then(function(text1){
             cy.log(text1.text())
-            cy.writeFile('arquivos/usd-brl-abertura.txt', text1.text(), {flag: "a+"})
+            cy.writeFile('arquivos/usd-brl-segunda-abertura.txt', text1.text(), {flag: "a+"})
             })
 
             //maxima 
             cy.get('.p-0 > .w-full > tbody > :nth-child(2) > :nth-child(4)').then(function(text2){
             cy.log(text2.text())
-            cy.writeFile('arquivos/usd-brl-max.txt', text2.text(), {flag: "a+"})
+            cy.writeFile('arquivos/usd-brl-segunda-max.txt', text2.text(), {flag: "a+"})
             })
 
             //minima 
             cy.get('.w-full > tbody > :nth-child(2) > :nth-child(5)').then(function(text3){
             cy.log(text3.text())
-            cy.writeFile('arquivos/usd-brl-min.txt', text3.text(), {flag: "a+"})
+            cy.writeFile('arquivos/usd-brl-segunda-min.txt', text3.text(), {flag: "a+"})
             })
  
     })
