dfc5eacc372e16033e02e956c9e11003035befc6
cmetrade <cmetrade.6l@gmail.com>
2024-02-20 21:49:39 -0300

    1

--- pys/ptax-futuro.py
+++ pys/ptax-futuro.py
@@ -2,7 +2,7 @@ import pandas as pd
 import time
 import os
 
-os.system("awk {'print $1'} arquivos/ptax-futuro.tx > arquivos/ptax-futuro2.txt")
+os.system("awk {'print $1'} arquivos/ptax-futuro.txt > arquivos/ptax-futuro2.txt")
 with open("arquivos/ptax-futuro2.txt", "r") as arquivo:
         result = arquivo.read()
 print(result)
