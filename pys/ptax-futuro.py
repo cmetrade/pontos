3baf130aeba0911f112f14b8256af1bbf8eedf68
cmetrade <cmetrade.6l@gmail.com>
2024-02-22 10:23:36 -0300

    q

--- pys/ptax-futuro.py
+++ pys/ptax-futuro.py
@@ -11,5 +11,5 @@ import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
 message = "PTAX-FUTURO = "
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {ptax1} + {ptax2}"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {ptax1} {ptax2}"
 print(requests.get(url).json()) # this sends the message
