71489a626c85a05b001e9cced730dc38f5e59d00
cmetrade <cmetrade.6l@gmail.com>
2024-02-22 10:20:47 -0300

    q

--- pys/ptax-futuro.py
+++ pys/ptax-futuro.py
@@ -7,10 +7,9 @@ with open("arquivos/ptax.txt", "r") as arquivo:
         ptax1 = arquivo.read()
 with open("arquivos/ptax-futuro2.txt", "r") as arquivo:
         ptax2 = arquivo.read()
-print(ptax1 + ptax2)
 import requests
 TOKEN = "5779297459:AAE2k4xaLnQZW0MRSmu0OX3UYftw7vZishg"
 chat_id = "-1001546918854"
 message = "PTAX-FUTURO = "
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {result}"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message} {ptax1} + {ptax2}"
 print(requests.get(url).json()) # this sends the message
