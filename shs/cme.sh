56de3954add27a8e0eeae62d31735cc55b26ec51
cmetrade <cmetrade.6l@gmail.com>
2024-02-22 21:20:17 -0300

    q

--- shs/cme.sh
+++ shs/cme.sh
@@ -8,8 +8,8 @@ i=1
 
 while [[ $i -lt 15 ]] 
 do
-        #mv package.json package.json.bkp
-        #mv package-cme.json package.json
+        mv package.json package.json.bkp
+        mv package-cme.json package.json
         npm install
         npm start
         if [ "$?" == '0' ]
@@ -18,8 +18,8 @@ do
                 python3 pys/cme-abertura.py
                 python3 pys/cme-high.py
                 python3 pys/cme-low.py
-                #mv package.json package-cme.json
-                #mv package.json.bkp package.json
+                mv package.json package-cme.json
+                mv package.json.bkp package.json
            break
            fi
         fi
