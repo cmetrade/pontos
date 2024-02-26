46afb5d344b3ed151d683911674b066018a9adc7
cmetrade <cmetrade.6l@gmail.com>
2024-02-26 20:58:58 -0300

    w

---
+++ shs/usd-brl-format.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+cd ~/pontos/arquivos
+ echo "DATA                    ABE                MAX               MIN" > usd-brl.txt
+ echo "$(cat linha1-usd-brl-data.txt) $(echo '        ') $(cat linha1-usd-brl-abertura.txt)  $(echo '        ') $(cat linha1-usd-brl-max.txt)  $(echo '        ') $(cat linha1-usd-brl-min.txt)" >> usd-brl.txt
+ echo "$(cat linha2-usd-brl-data.txt) $(echo '        ') $(cat linha2-usd-brl-abertura.txt)  $(echo '        ') $(cat linha2-usd-brl-max.txt)  $(echo '        ') $(cat linha2-usd-brl-min.txt)" >> usd-brl.txt
--- shs/usd-brl.sh
+++ shs/usd-brl.sh
@@ -2,38 +2,17 @@
 #
 cd ~/pontos
 mv cypress/e2e/* cypress/
-dia_semana="Monday"
-dia=`date +"%A"`
-
-
-if [[ $dia == "$dia_semana" ]] ; then
-   mv cypress/usd-brl-segunda.cy.js cypress/e2e/
-   npm install
-   i=1
-        while [[ $i -lt 11 ]] 
-        do
-        npm start
-                if [ "$?" == '0' ]
-                then
-                        python3 pys/usd-brl-segunda.py
-                       # exit 1
-                       break
-                fi
-        echo "$i tentativa(s)"
-        ((i++))
-        done
-        echo "$i" > logs/usd-brl-segunda.log
-else
-
 mv cypress/usd-brl.cy.js cypress/e2e/
+
 npm install
 i=1
 
-        while [[ $i -lt 11 ]] 
+        while [[ $i -lt 11 ]]
         do
         npm start
         if [ "$?" == '0' ]
            then
+                ./shs/usd-brl-format.sh
                 python3 pys/usd-brl.py
                 break
         fi
