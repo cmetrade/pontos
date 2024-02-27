cb2211f83887ed8a96c277074b4b392724461b44
cmetrade <cmetrade.6l@gmail.com>
2024-02-26 21:32:26 -0300

    w

--- shs/usd-brl-format.sh
+++ shs/usd-brl-format.sh
@@ -4,3 +4,4 @@ cd ~/pontos/arquivos
  echo "DATA                    ABE                MAX               MIN" > usd-brl.txt
  echo "$(cat linha1-usd-brl-data.txt) $(echo '        ') $(cat linha1-usd-brl-abertura.txt)  $(echo '        ') $(cat linha1-usd-brl-max.txt)  $(echo '        ') $(cat linha1-usd-brl-min.txt)" >> usd-brl.txt
  echo "$(cat linha2-usd-brl-data.txt) $(echo '        ') $(cat linha2-usd-brl-abertura.txt)  $(echo '        ') $(cat linha2-usd-brl-max.txt)  $(echo '        ') $(cat linha2-usd-brl-min.txt)" >> usd-brl.txt
+ echo "$(cat linha3-usd-brl-data.txt) $(echo '        ') $(cat linha3-usd-brl-abertura.txt)  $(echo '        ') $(cat linha3-usd-brl-max.txt)  $(echo '        ') $(cat linha3-usd-brl-min.txt)" >> usd-brl.txt
