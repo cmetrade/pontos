8dc6d17247d626e4d3614dc4332d70245ca727be
cmetrade <cmetrade.6l@gmail.com>
2024-02-26 21:33:00 -0300

    w

--- cypress/e2e/usd-brl.cy.js
+++ cypress/e2e/usd-brl.cy.js
@@ -4,6 +4,8 @@ describe('USD-BRL', function (){
     it('USD-BRL', function(){
        cy.visit('https://br.investing.com/currencies/usd-brl-historical-data')
 
+
+            // LINHA 1
             //abertura 
             cy.get('.p-0 > .w-full > tbody > :nth-child(1) > :nth-child(3)').then(function(text1){
             cy.log(text1.text())
@@ -28,6 +30,8 @@ describe('USD-BRL', function (){
             cy.writeFile('arquivos/linha1-usd-brl-data.txt', text4.text(), {flag: "a+"})
             })
             
+
+            // LINHA 2
             //abertura 
             cy.get('.p-0 > .w-full > tbody > :nth-child(2) > :nth-child(3)').then(function(text5){
             cy.log(text5.text())
@@ -50,7 +54,32 @@ describe('USD-BRL', function (){
             cy.get(':nth-child(2) > .text-left > time').then(function(text8){
             cy.log(text8.text())
             cy.writeFile('arquivos/linha2-usd-brl-data.txt', text8.text(), {flag: "a+"})
-            })    
+            }) 
+
+            //LINHA 3
+            //abertura 
+            cy.get('.p-0 > .w-full > tbody > :nth-child(3) > :nth-child(3)').then(function(text6){
+            cy.log(text6.text())
+            cy.writeFile('arquivos/linha3-usd-brl-abertura.txt', text6.text(), {flag: "a+"})
+            })
+
+            //maxima 
+            cy.get('.p-0 > .w-full > tbody > :nth-child(3) > :nth-child(4)').then(function(text7){
+            cy.log(text7.text())
+            cy.writeFile('arquivos/linha3-usd-brl-max.txt', text7.text(), {flag: "a+"})
+            })
+
+            //minima 
+            cy.get('.w-full > tbody > :nth-child(3) > :nth-child(5)').then(function(text8){
+            cy.log(text8.text())
+            cy.writeFile('arquivos/linha3-usd-brl-min.txt', text8.text(), {flag: "a+"})
+            })
+
+            //data
+            cy.get(':nth-child(3) > .text-left > time').then(function(text9){
+            cy.log(text9.text())
+            cy.writeFile('arquivos/linha3-usd-brl-data.txt', text9.text(), {flag: "a+"})
+            })     
     })
 })
    
