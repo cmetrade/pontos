c236241f4057cb33ae5ce3dcbeb9c473ad414fd8
cmetrade <cmetrade.6l@gmail.com>
2024-02-26 21:00:34 -0300

    w

--- pys/usd-brl.py
+++ pys/usd-brl.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import time
 import os
-os.system("awk {'print $1'} arquivos/usd-brl-abertura.txt arquivos/usd-brl-max.txt arquivos/usd-brl-min.txt > arquivos/usd-brl.txt")
+#os.system("awk {'print $1'} arquivos/usd-brl-abertura.txt arquivos/usd-brl-max.txt arquivos/usd-brl-min.txt > arquivos/usd-brl.txt")
 
 with open("arquivos/usd-brl.txt", "r") as arquivo:
         result = arquivo.read()
