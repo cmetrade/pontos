e0c61f9cad70b05b35f9bf86c5a5cdc366693459
cmetrade <cmetrade.6l@gmail.com>
2024-05-03 13:27:17 -0300

    1

--- cypress/e2e/wdo-fecha.cy.js
+++ cypress/e2e/wdo-fecha.cy.js
@@ -1,10 +1,31 @@
 /// <reference types="cypress" />
 
 
-describe('WDOH24', function (){
+describe('WDO', function (){
     //Cypress.config('defaultCommandTimeout', 100000);
     it('capturar dados wdo', function(){
-        cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOM24/historico')
+        const dayjs = require('dayjs')
+        const mes = dayjs().format('M')
+        const ano = dayjs().format('YY')
+
+        const object1 = {
+            a: 'F',
+            b: 'G',
+            c: 'H',
+            d: 'J',
+            e: 'K',
+            f: 'M',
+            g: 'N',
+            h: 'Q',
+            i: 'U',
+            j: 'V',
+            k: 'X',
+            l: 'Z',
+          };
+          
+        var letra = Object.values(object1)[mes] 
+        cy.visit(`https://br.advfn.com/bolsa-de-valores/bmf/WDO${letra}${ano}/historico`)
+       // cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOM24/historico')
             
         cy.get(':nth-child(8) > table > tbody > :nth-child(2) > :nth-child(2)').then(function(text2){
                cy.log(text2.text())
--- cypress/e2e/wdoD-1.cy.js
+++ cypress/e2e/wdoD-1.cy.js
@@ -1,8 +1,29 @@
 /// <reference types="cypress" />
 
-describe('WDOH - Abertura D -1', function (){
+describe('WDO - Abertura D -1', function (){
     it('capturar dados de abertura wdo', function(){
-        cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOM24/historico')
+        const dayjs = require('dayjs')
+        const mes = dayjs().format('M')
+        const ano = dayjs().format('YY')
+
+        const object1 = {
+            a: 'F',
+            b: 'G',
+            c: 'H',
+            d: 'J',
+            e: 'K',
+            f: 'M',
+            g: 'N',
+            h: 'Q',
+            i: 'U',
+            j: 'V',
+            k: 'X',
+            l: 'Z',
+          };
+        var letra = Object.values(object1)[mes]
+
+        cy.visit(`https://br.advfn.com/bolsa-de-valores/bmf/WDO${letra}${ano}/historico`)
+        //cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOM24/historico')
        
         cy.get(':nth-child(8) > table > tbody > :nth-child(3) > :nth-child(2)').then(function(text2){
               cy.log(text2.text())
