6cef2ba67c8f37257c5c4499a3e097e8be44d8d0
cmetrade <cmetrade.6l@gmail.com>
2024-08-16 12:02:42 -0300

    1

--- cypress/e2e/wdo-fecha.cy.js
+++ cypress/e2e/wdo-fecha.cy.js
@@ -27,7 +27,8 @@ describe('WDO', function (){
         cy.visit(`https://br.advfn.com/bolsa-de-valores/bmf/WDO${letra}${ano}/historico`)
        // cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOM24/historico')
             
-        cy.get(':nth-child(8) > table > tbody > :nth-child(2) > :nth-child(2)').then(function(text2){
+        //cy.get(':nth-child(8) > table > tbody > :nth-child(2) > :nth-child(2)').then(function(text2){
+        cy.get('[comp-id="164"] > .ag-cell-wrapper > .ag-cell-value').then(function(text2){    
                cy.log(text2.text())
                cy.writeFile('arquivos/wdo-fecha.txt', text2.text())
            })
