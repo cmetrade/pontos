26fc21f94173096714fab625960a12c1fdadb891
cmetrade <cmetrade.6l@gmail.com>
2024-08-20 11:24:56 -0300

    1

--- cypress/e2e/wdoD-2.cy.js
+++ cypress/e2e/wdoD-2.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('WDO - fechamento D -2', function (){
+describe('WDO - fechamento D -1', function (){
     it('capturar dados de fechamento wdo', function(){
         const dayjs = require('dayjs')
         const mes = dayjs().format('M')
@@ -23,8 +23,7 @@ describe('WDO - fechamento D -2', function (){
         var letra = Object.values(object1)[mes]
 
         cy.visit(`https://br.advfn.com/bolsa-de-valores/bmf/WDO${letra}${ano}/historico`)
-        //cy.visit('https://br.advfn.com/bolsa-de-valores/bmf/WDOM24/historico')
-        cy.get('[comp-id="170"] > .ag-cell-wrapper > .ag-cell-value').then(function(text2){
+        cy.get('[comp-id="164"] > .ag-cell-wrapper > .ag-cell-value').then(function(text2){
        // cy.get(':nth-child(8) > table > tbody > :nth-child(3) > :nth-child(2)').then(function(text2){
               cy.log(text2.text())
               cy.writeFile('arquivos/wdoD-2.txt', text2.text())
